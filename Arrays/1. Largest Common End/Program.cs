296da2e94f4a47ed2fd43d59be9d86a8ae3ee500
MitkoGeorgievGeorgiev <mito_vr@abv.bg>
2018-03-06 18:57:59 +0200

    first commit

---
+++ Arrays/1. Largest Common End/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace _1.Largest_Common_End
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string[] firstRow= Console.ReadLine().Split(' ');
+           string[] secondRow = Console.ReadLine().Split(' ');
+            int counter = 0;
+            int lenght = Math.Min(firstRow.Length, secondRow.Length);
+            for (int i = 0; i <lenght; i++)
+            {
+                if (firstRow[i]==secondRow[i])
+                {
+                    counter++;
+                }
+                else if (firstRow[i] != secondRow[i])
+                {
+                    break;
+                }
+            }
+            firstRow = firstRow.Reverse().ToArray();
+            secondRow = secondRow.Reverse().ToArray();
+            int secondCounter = 0;
+            for (int i = 0; i < lenght; i++)
+            {
+                if (firstRow[i] == secondRow[i])
+                {
+                    secondCounter++;
+                }
+                else if (firstRow[i] != secondRow[i])
+                {
+                    break;
+                }
+            }
+
+
+            Console.WriteLine(Math.Max(counter,secondCounter));
+
+        }
+    }
+}
